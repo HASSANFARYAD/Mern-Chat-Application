4b5a226dfd56e5444042d8fabb55cb62904460ab
Has San <uzair.aslam02@gmail.com>
2023-12-23 10:51:05 -0600

    Base Url Changed

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://phenomenal-truffle-6b1bd9.netlify.app/";
+export const host = "https://phenomenal-truffle-6b1bd9.netlify.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
